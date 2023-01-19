d28c03e3f427b4d3d169f687174f3f1fa701032a
reisalazar <rrsb45@gmail.com>
2023-01-19 16:36:44 -0300

    Using Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -12,17 +14,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
-              .copyWith(secondary: Colors.deepOrange),
-          fontFamily: 'Lato'),
-      home: ProductsOverviewPage(),
-      routes: {
-        AppRoutes.productDetail: (context) => const ProductDetailPage(),
-      },
-      debugShowCheckedModeBanner: false,
+    return ChangeNotifierProvider(
+      create: (context) => ProductList(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+                .copyWith(secondary: Colors.deepOrange),
+            fontFamily: 'Lato'),
+        home: ProductsOverviewPage(),
+        routes: {
+          AppRoutes.productDetail: (context) => const ProductDetailPage(),
+        },
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
