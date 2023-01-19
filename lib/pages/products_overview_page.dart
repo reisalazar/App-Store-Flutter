9a0c56ddcc4df5641d65ffb9818c841f663b84ba
reisalazar <rrsb45@gmail.com>
2023-01-19 16:42:08 -0300

    extracting grid

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,33 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/components/product_item.dart';
-import 'package:shop/data/dummy_data.dart';
-import 'package:shop/models/product.dart';
-import 'package:shop/models/product_list.dart';
+import '../components/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        itemBuilder: (contex, index) =>
-            ProductItem(product: loadedProducts[index]),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: const ProjectGrid(),
     );
   }
 }
