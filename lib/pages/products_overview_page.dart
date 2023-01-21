b24f46208239f18a8577042206d351ce870faa17
reisalazar <rrsb45@gmail.com>
2023-01-21 12:01:26 -0300

    provider to cart

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/badge.dart';
 import 'package:shop/components/product_grid.dart';
+
+import '../models/cart.dart';
 
 enum FilterOptions {
   favorite,
@@ -43,6 +47,16 @@ class _ProductsOverviewPageState extends State<ProductsOverviewPage> {
                 }
               });
             },
+          ),
+          Consumer<Cart>(
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.shopping_cart),
+            ),
+            builder: (context, cart, child) => Badge(
+              value: cart.itemsCount.toString(),
+              child: child!,
+            ),
           ),
         ],
       ),
