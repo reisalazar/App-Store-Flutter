3ec99983aaa9edc3b225e972e9b555c49e6d93bd
reisalazar <rrsb45@gmail.com>
2023-01-21 14:21:02 -0300

    cart item

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
-
-import '../models/cart_item.dart';
+import 'package:shop/models/cart_item.dart';
 
 class CartItemWidget extends StatelessWidget {
-  const CartItemWidget(this.cartItem, {super.key});
-
   final CartItem cartItem;
+
+  const CartItemWidget(this.cartItem, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text(cartItem.name);
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 4,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            foregroundColor: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: FittedBox(
+                child: Text('${cartItem.price}'),
+              ),
+            ),
+          ),
+          title: Text(cartItem.name),
+          subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quantity}'),
+          trailing: Text('${cartItem.quantity}x'),
+        ),
+      ),
+    );
   }
 }
