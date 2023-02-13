39c4fb87cd4c1ba3a6c684d5fce6dfff870cf38c
reisalazar <rrsb45@gmail.com>
2023-02-13 02:05:19 -0300

    Input Url

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -10,12 +10,25 @@ class ProductFormPage extends StatefulWidget {
 class _ProductFormPageState extends State<ProductFormPage> {
   final _priceFocus = FocusNode();
   final _descriptionFocus = FocusNode();
+  final _imageUrlFocus = FocusNode();
+  final _imageUrlController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocus.addListener(updateImage);
+  }
 
   @override
   void dispose() {
     super.dispose();
     _priceFocus.dispose();
     _descriptionFocus.dispose();
+    _imageUrlFocus.removeListener(updateImage);
+  }
+
+  void updateImage() {
+    setState(() {});
   }
 
   @override
@@ -52,6 +65,39 @@ class _ProductFormPageState extends State<ProductFormPage> {
               focusNode: _descriptionFocus,
               keyboardType: TextInputType.multiline,
               maxLines: 3,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration:
+                        const InputDecoration(labelText: 'Url da Imagem'),
+                    textInputAction: TextInputAction.done,
+                    focusNode: _imageUrlFocus,
+                    keyboardType: TextInputType.url,
+                    controller: _imageUrlController,
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: const EdgeInsets.only(
+                    top: 10,
+                    left: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 1),
+                  ),
+                  alignment: Alignment.center,
+                  child: _imageUrlController.text.isEmpty
+                      ? const Text('Informe a Url')
+                      : FittedBox(
+                          fit: BoxFit.cover,
+                          child: Image.network(_imageUrlController.text),
+                        ),
+                )
+              ],
             ),
           ],
         )),
