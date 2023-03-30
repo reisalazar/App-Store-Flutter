13b5357600135f353db4135643735055f2e61072
reisalazar <rrsb45@gmail.com>
2023-03-30 09:07:58 -0300

    Progress indicator

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -18,7 +18,7 @@ class ProductList with ChangeNotifier {
     return _items.length;
   }
 
-  void saveProduct(Map<String, Object> data) {
+  Future<void> saveProduct(Map<String, Object> data) {
     bool hasId = data['id'] != null;
 
     final product = Product(
@@ -30,13 +30,13 @@ class ProductList with ChangeNotifier {
     );
 
     if (hasId) {
-      updateProduct(product);
+      return updateProduct(product);
     } else {
-      addProduct(product);
+      return addProduct(product);
     }
   }
 
-  void addProduct(Product product) {
+  Future<void> addProduct(Product product) {
     final future = http.post(
       Uri.parse('_baseUrl/products.json'),
       body: jsonEncode(
@@ -49,7 +49,7 @@ class ProductList with ChangeNotifier {
         },
       ),
     );
-    future.then(
+    return future.then<void>(
       (response) {
         final id = jsonDecode(response.body)['name'];
         _items.add(
@@ -67,13 +67,15 @@ class ProductList with ChangeNotifier {
     );
   }
 
-  void updateProduct(Product product) {
+  Future<void> updateProduct(Product product) {
     int index = _items.indexWhere((element) => element.id == product.id);
 
     if (index >= 0) {
       _items[index] = product;
       notifyListeners();
     }
+
+    return Future.value();
   }
 
   void removeProduct(Product product) {
