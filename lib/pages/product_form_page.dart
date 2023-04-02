0f66e720f3355c7877231274d0cd5a6093bf70a3
reisalazar <rrsb45@gmail.com>
2023-04-02 00:28:05 -0300

    Error handling

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -86,7 +86,21 @@ class _ProductFormPageState extends State<ProductFormPage> {
     Provider.of<ProductList>(
       context,
       listen: false,
-    ).saveProduct(_formData).then((value) {
+    ).saveProduct(_formData).catchError((error) {
+      return showDialog<void>(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Ocorreu um erro!'),
+          content: const Text('Erro ao salvar o produto.'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Ok'),
+            )
+          ],
+        ),
+      );
+    }).then((value) {
       setState(() {
         _isLoading = false;
       });
