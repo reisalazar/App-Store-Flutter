f7402732e9b664efa17d3598d57f8930dfdf7c24
reisalazar <rrsb45@gmail.com>
2023-04-05 09:16:40 -0300

    Pull to refresh implm

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -8,6 +8,10 @@ import 'package:shop/utils/app_routes.dart';
 class ProductsPage extends StatelessWidget {
   const ProductsPage({super.key});
 
+  Future<void> _refreshProducts(BuildContext context) {
+    return Provider.of<ProductList>(context, listen: false).loadProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final ProductList products = Provider.of(context);
@@ -20,20 +24,23 @@ class ProductsPage extends StatelessWidget {
             onPressed: () {
               Navigator.of(context).pushNamed(AppRoutes.productsForm);
             },
-            icon: Icon(Icons.add),
+            icon: const Icon(Icons.add),
           ),
         ],
       ),
       drawer: const AppDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: ListView.builder(
-          itemCount: products.itemsCount,
-          itemBuilder: (context, index) => Column(
-            children: [
-              ProductItem(products.items[index]),
-              const Divider(),
-            ],
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListView.builder(
+            itemCount: products.itemsCount,
+            itemBuilder: (context, index) => Column(
+              children: [
+                ProductItem(products.items[index]),
+                const Divider(),
+              ],
+            ),
           ),
         ),
       ),
