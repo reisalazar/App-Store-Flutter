d384bdf2be1c64df158bf706026dd94c960aa771
reisalazar <rrsb45@gmail.com>
2023-04-07 18:15:58 -0300

    loading products

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -2,25 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order.dart';
+import 'package:shop/models/order_list.dart';
 
-import '../models/order_list.dart';
+class OrdersPage extends StatefulWidget {
+  const OrdersPage({Key? key}) : super(key: key);
 
-class OrdersPage extends StatelessWidget {
-  const OrdersPage({super.key});
+  @override
+  State<OrdersPage> createState() => _OrdersPageState();
+}
+
+class _OrdersPageState extends State<OrdersPage> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<OrderList>(
+      context,
+      listen: false,
+    ).loadOrders().then((_) {
+      setState(() => _isLoading = false);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final OrderList orders = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus Pedidos'),
       ),
       drawer: const AppDrawer(),
-      body: ListView.builder(
-        itemCount: orders.itemsCount,
-        itemBuilder: (context, index) =>
-            OrderWidget(order: orders.items[index]),
-      ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: orders.itemsCount,
+              itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+            ),
     );
   }
 }
