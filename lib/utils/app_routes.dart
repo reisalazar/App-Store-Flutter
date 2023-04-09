fa80a1bdba54af1161b63eb0b4902f49d25a53db
reisalazar <rrsb45@gmail.com>
2023-04-09 09:20:31 -0300

    Auth page + cascade operator

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,10 +1,11 @@
 import 'package:shop/models/order.dart';
 
 class AppRoutes {
-  static const productDetail = '/product-detail';
-  static const home = '/';
+  static const auth = '/';
+  static const home = '/home';
   static const order = '/orders';
   static const cart = '/cart';
   static const products = '/products';
   static const productsForm = '/products-form';
+  static const productDetail = '/product-detail';
 }
